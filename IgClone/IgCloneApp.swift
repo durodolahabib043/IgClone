6cda1cf40ad73eb3c6ed61a06d9187f2b341bb54
durodolahabib043 <durodolahabib043@gmail.com>
2025-01-28 13:16:31 -0500

    first commit

---
+++ IgClone/IgCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  IgCloneApp.swift
+//  IgClone
+//
+//  Created by Habib Durodola on 1/27/25.
+//
+
+import SwiftUI
+
+@main
+struct IgCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
